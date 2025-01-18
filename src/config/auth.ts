e51da56d245d2d7fd025c14f193b0884e7a618f3
Rali <rayanedu92290@gmail.com>
2025-01-18 18:42:16 +0100

    basic setup

---
+++ src/config/auth.ts
@@ -0,0 +1,23 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+import prisma from "./prisma";
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+};
+
+export const { auth, signIn, signOut } = NextAuth(authOptions);
